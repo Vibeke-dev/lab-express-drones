fd40a30678f74fa520d23069e9856d3cea0f7a21
Vibeke-dev <vibeke.granhoej.joergensen@lego.com>
2022-01-31 16:44:15 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,74 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drones = require("../models/Drone.model.js"); 
 
+//READ
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drones.find()
+    .then((allTheDronesFromDB) => {
+      // -> allTheBooksFromDB is a placeholder, it can be any word
+      console.log("Retrieved drones from DB:", allTheDronesFromDB);
+
+      res.render("drones/list.hbs", { drones: allTheDronesFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the books from the DB: ", error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
 });
 
+//CREATE
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  //console.log(req.body);
+  const { name, propellers, maxSpeed } = req.body;
+ 
+  Drones.create({ name, propellers, maxSpeed })
+  .then(() => res.redirect('/drones'))
+  //.catch(error => next(error));
+  .catch(error => res.render('drones/create-form.hbs'));
 });
 
+//UPDATE
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drones.findById(id)
+    .then(droneToEdit => {
+      res.render('drones/update-form.hbs', { drone: droneToEdit }); // <-- add this line
+    })
+    .catch(error => next(error));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+ 
+  Drones.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+    .then(updatedDrone => res.redirect('/drones')) 
+    //.catch(error => next(error));
+    .catch(error => res.render('drones/update-form.hbs', { drone: droneToEdit }));
 });
 
+//DELETE
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+ 
+  Drones.findByIdAndDelete(id)
+    .then(() => res.redirect('/drones'))
+    .catch(error => next(error));
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,30 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drones = require("../models/Drone.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// PASTE HERE THE LIST OF BOOKS PROVIDED IN THIS GIST: https://gist.github.com/ironhack-edu/2816267a015d4870f95275cb873d33b6
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+Drones.create(drones)
+  .then((booksFromDB) => {
+    console.log(`Created ${booksFromDB.length} drones`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch((err) =>
+    console.log(`An error occurred while creating books from the DB: ${err}`)
+  );
